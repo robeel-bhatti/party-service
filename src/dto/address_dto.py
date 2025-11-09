850271175aecbb46fa27b32dc190126b7c34d999
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-11-09 12:46:18 -0500

    test: add units for new address dto methods.

--- src/dto/address_dto.py
+++ src/dto/address_dto.py
@@ -1,3 +1,4 @@
+import hashlib
 from typing import Optional, Self
 
 from pydantic import BaseModel, ConfigDict, Field, model_validator
@@ -20,9 +21,38 @@ class AddressDTO(BaseModel):
     @model_validator(mode="after")
     def parse_state(self) -> Self:
         try:
-            USState(self.state)
+            USState(self.state.upper())
         except ValueError:
             raise ValueError(
                 f"Failed to create Party. An invalid US state code: '{self.state}' was provided."
             )
         return self
+
+    @model_validator(mode="after")
+    def normalize_fields(self) -> Self:
+        for name in AddressDTO.model_fields.keys():
+            val = getattr(self, name)
+            if val:
+                val = val.strip()
+                if name == "state" or name == "country":
+                    val = val.upper()
+                elif name == "city":
+                    val = val.capitalize()
+                elif name == "street_one" or name == "street_two":
+                    val = val.title()
+
+            setattr(self, name, val)
+
+        return self
+
+    def get_hash(self) -> str:
+        """Normalize address components, then get a deterministic hash."""
+        normalized_string = (
+            f"{self.street_one}"
+            f"|{self.street_two if self.street_two else ''}"
+            f"|{self.city}"
+            f"|{self.state}"
+            f"|{self.postal_code}"
+            f"|{self.country}"
+        )
+        return hashlib.sha256(normalized_string.encode()).hexdigest()
--- src/service/party_service.py
+++ src/service/party_service.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from src.dto.party_dto import PartyDTO
 from src.models.address import Address
 from src.models.party import Party
 from src.repository.abstract_repository import AbstractRepository
@@ -11,3 +14,13 @@ class PartyService:
     ):
         self.party_repository = party_repository
         self.address_repository = address_repository
+
+    def add_party(self, req: dict[str, Any]) -> None:
+        # validate request payload first
+        party = PartyDTO(**req)
+
+        # if request is valid, start address validation
+        # to do address validation, we need to get the address hash
+        party.address.get_hash()
+
+        # then we will query the database for address row with that hash
