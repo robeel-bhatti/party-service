da8715471a657f2822d3b02f2dc226e5be5d389f
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-11-09 22:08:52 -0500

    fix: add uow repo to DI container.py.

--- src/config/container.py
+++ src/config/container.py
@@ -7,6 +7,7 @@ from src.models.party import Party
 from src.repository.abstract_repository import AbstractRepository
 from src.repository.address_repository import AddressRepository
 from src.repository.party_repository import PartyRepository
+from src.repository.unit_of_work import UnitOfWork
 from src.service.party_service import PartyService
 
 
@@ -16,6 +17,7 @@ class Container:
         self._party_repository: Optional[AbstractRepository[Party]] = None
         self._address_repository: Optional[AbstractRepository[Address]] = None
         self._party_service: Optional[PartyService] = None
+        self._unit_of_work: Optional[UnitOfWork] = None
 
     @property
     def party_repository(self) -> AbstractRepository[Party]:
@@ -36,3 +38,11 @@ class Container:
                 self.party_repository, self.address_repository
             )
         return self._party_service
+
+    @property
+    def unit_of_work(self) -> UnitOfWork:
+        if not self._unit_of_work:
+            self._unit_of_work = UnitOfWork(
+                self.db_session, self.party_repository, self.address_repository
+            )
+        return self._unit_of_work
