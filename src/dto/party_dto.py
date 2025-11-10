23fad239b1e86a9ceb3b70e88ddf76dc3e54626a
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-11-09 22:04:43 -0500

    fix: align field constraints to be consistent with db constraints.

--- src/dto/party_dto.py
+++ src/dto/party_dto.py
@@ -1,6 +1,6 @@
-from typing import Optional, Self
+from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, EmailStr, model_validator
+from pydantic import BaseModel, ConfigDict, EmailStr
 from pydantic.alias_generators import to_camel
 
 from src.dto.address_dto import AddressDTO
@@ -11,15 +11,7 @@ class PartyDTO(BaseModel):
     model_config = ConfigDict(alias_generator=to_camel)
     first_name: GeneralStringConstraint
     middle_name: Optional[GeneralStringConstraint] = None
-    last_name: Optional[GeneralStringConstraint] = None
-    email: Optional[EmailStr] = None
-    phone_number: Optional[PhoneType] = None
+    last_name: GeneralStringConstraint
+    email: EmailStr
+    phone_number: PhoneType
     address: AddressDTO
-
-    @model_validator(mode="after")
-    def check_fields(self) -> Self:
-        if self.email is None and self.phone_number is None:
-            raise ValueError(
-                "Failed to create Party. Either email or phone_number must be provided."
-            )
-        return self
