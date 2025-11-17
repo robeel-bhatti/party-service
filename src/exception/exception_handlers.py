1d52ae04854ff2c5b5ff93516357002b9be31c50
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-11-16 21:02:41 -0500

    fix: remove unnecessary exception handling in validation error handling.

--- src/exception/exception_handlers.py
+++ src/exception/exception_handlers.py
@@ -4,7 +4,6 @@ from http import HTTPStatus
 from flask import Response, jsonify, request
 from pydantic import ValidationError
 
-from src.exception.custom_exceptions import EntityAlreadyExistsError
 from src.exception.custom_exceptions import ErrorDTO
 
 logger = logging.getLogger(__name__)
@@ -14,37 +13,19 @@ def handle_validation_error(e: ValidationError) -> tuple[Response, int]:
     """Handle validation errors that occur during Pydantic model validation."""
 
     logger.error(f"The request failed due to the following validation error: {e}")
-    if e.errors():
-        validation_errors = []
+    validation_errors = []
 
-        for error in e.errors():
-            bad_field = ".".join(str(error["loc"]))
-            validation_errors.append(
-                f"Invalid value: {error['input']} for field '{bad_field}'. {error['msg']}."
-            )
+    for error in e.errors():
+        bad_field = ".".join(str(error["loc"]))
+        validation_errors.append(
+            f"Invalid value: {error['input']} for field '{bad_field}'. {error['msg']}."
+        )
 
-        error_dto = ErrorDTO(
-            status=HTTPStatus.UNPROCESSABLE_ENTITY.value,
-            title=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
-            detail=f"Validation failed with ({len(validation_errors)}) errors",
-            instance=request.path,
-        ).to_dict(validation_errors=validation_errors)
-
-        return jsonify(error_dto), 422
-
-    logger.error(
-        f"Could not process ValidationError because of invalid handler input: {e.errors}"
-    )
-    return jsonify({"message": "An unexpected error occurred processing request"}), 500
-
-
-def handle_entity_already_exists_error(
-    e: EntityAlreadyExistsError,
-) -> tuple[Response, int]:
     error_dto = ErrorDTO(
-        status=HTTPStatus.CONFLICT.value,
-        title=HTTPStatus.CONFLICT.phrase,
-        detail=str(e),
+        status=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        title=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        detail=f"Validation failed with ({len(validation_errors)}) errors",
         instance=request.path,
-    )
-    return jsonify(error_dto), 409
+    ).to_dict(validation_errors=validation_errors)
+
+    return jsonify(error_dto), 422
