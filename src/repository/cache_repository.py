d9574f756f777f9ad1e594b6d8e21a0fda0dc12a
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-11-19 23:47:45 -0500

    docs: add docstrings for the CacheRepository class.

--- src/repository/cache_repository.py
+++ src/repository/cache_repository.py
@@ -1,7 +1,6 @@
 from redis import Redis
-from pydantic import BaseModel
 import logging
-from typing import Awaitable
+from typing import Awaitable, Any
 from src.config.constants import AppConstants
 from src.config.enums import ServiceEntities
 
@@ -9,10 +8,18 @@ logger = logging.getLogger(__name__)
 
 
 class CacheRepository:
+    """Responsible for interacting with the external Cache being used by this application for faster reads."""
+
     def __init__(self, cache: Redis) -> None:
         self._cache = cache
 
-    def add(self, id: int, entity: ServiceEntities, value: BaseModel) -> None:
+    def add(self, id: int, entity: ServiceEntities, value: Any) -> None:
+        """
+        Add an entity to the cache.
+        :param id: The unique identifier (ex. primary key) for the entity.
+        :param entity: An enum identifying the entity being stored.
+        :param value: The attributes of the entity.
+        """
         bytes_val = value.model_dump_json().encode("utf-8")
         self._cache.set(
             self._generate_key(id, entity), bytes_val
@@ -20,8 +27,21 @@ class CacheRepository:
         logger.info(f"Party with ID {id} saved in cache.")
 
     def get(self, id: int, entity: ServiceEntities) -> Awaitable[bytes]:
+        """
+        Get an entity from the cache.
+        :param id: The unique identifier (ex. primary key) for the entity.
+        :param entity: An enum identifying the entity being stored.
+        :return: The attributes of the entity.
+        """
         return self._cache.get(self._generate_key(id, entity))
 
     @staticmethod
     def _generate_key(id: int, entity: ServiceEntities) -> str:
+        """
+        Generate a unique cache key for the entity. The creation of this cache key
+        allows the cache to create a namespace or "folder" that belongs only to this service.
+        :param id: The unique identifier (ex. primary key) for the entity.
+        :param entity: An enum identifying the entity being stored.
+        :return: A unique cache key.
+        """
         return f"{AppConstants.APP_NAME}:{entity.value}:{id}"
