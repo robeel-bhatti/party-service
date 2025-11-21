0a20c816d249a71a86e604a0fb6b8f51341bf7af
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-11-21 00:22:17 -0500

    fix: clean up pydantic exception handler. (#26)
    fix: tidy up the error message that's returned from the pydantic exception handler.

--- src/exception/exception_handlers.py
+++ src/exception/exception_handlers.py
@@ -5,7 +5,7 @@ from flask import Response, jsonify, request
 from sqlalchemy.exc import SQLAlchemyError
 from pydantic import ValidationError
 
-from src.exception.custom_exceptions import ErrorDTO
+from src.exception.error_dto import ErrorDTO
 
 logger = logging.getLogger(__name__)
 
@@ -13,19 +13,18 @@ logger = logging.getLogger(__name__)
 def handle_validation_error(e: ValidationError) -> tuple[Response, int]:
     """Handle validation errors that occur during Pydantic model validation."""
 
-    logger.error(f"The request failed due to the following validation error: {e}")
-    validation_errors = []
-
-    for error in e.errors():
-        bad_field = ".".join(str(error["loc"]))
-        validation_errors.append(
-            f"Invalid value: {error['input']} for field '{bad_field}'. {error['msg']}."
-        )
+    logger.error(
+        f"The request failed due to the following validation error(s): {e.json(indent=4)}"
+    )
+    validation_errors = [
+        {"field": ".".join(str(loc) for loc in error["loc"]), "message": error["msg"]}
+        for error in e.errors()
+    ]
 
     error_dto = ErrorDTO(
         status=HTTPStatus.UNPROCESSABLE_ENTITY.value,
         title=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
-        detail=f"Validation failed with ({len(validation_errors)}) errors",
+        detail="Request validation failed",
         instance=request.path,
     ).to_dict(validation_errors=validation_errors)
 
@@ -35,7 +34,7 @@ def handle_validation_error(e: ValidationError) -> tuple[Response, int]:
 def handle_database_error(e: SQLAlchemyError) -> tuple[Response, int]:
     """Handle database errors that occur during the request."""
 
-    logger.error(f"The request failed due to the following database error: {e}")
+    logger.exception(f"The request failed due to the following database error: {e}")
     error_dto = ErrorDTO(
         status=HTTPStatus.INTERNAL_SERVER_ERROR.value,
         title=HTTPStatus.INTERNAL_SERVER_ERROR.phrase,
