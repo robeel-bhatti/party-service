555a5614093be998c13dd86c8480019d9b26fef7
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-11-23 22:20:17 -0500

    fix: migrate away from flask-smorest.

--- src/blueprints/party_blueprint.py
+++ src/blueprints/party_blueprint.py
@@ -1,21 +1,17 @@
 from typing import Any
 import logging
 
-from flask import current_app, request
+from flask import current_app, request, Blueprint
 from flask.views import MethodView
-from flask_smorest import Blueprint
 
-
-party_blp = Blueprint("party", __name__)
+party_blp = Blueprint("party_blueprint", __name__, url_prefix="/parties")
 logger = logging.getLogger(__name__)
 
 
-@party_blp.route("/parties")
 class Party(MethodView):
-    @party_blp.response(status_code=201)  # type: ignore[misc]
-    def post(self) -> dict[str, Any]:
+    def post(self) -> tuple[dict[str, Any], int]:
         """Handles REST requests to create a new party."""
 
         logger.info("POST /parties endpoint received request to create Party.")
         party_service = current_app.container.party_service
-        return party_service.add_party(request.json)  # type: ignore[no-any-return]
+        return party_service.add_party(request.json), 201
